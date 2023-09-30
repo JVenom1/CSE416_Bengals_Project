e9535974a4c014971418fe3e4ad2b69aa4cdad7e
Jeremy Lutsky <jlstring16@gmail.com>
2023-09-30 11:45:56 -0400

    added drop down menu of state selection also bug fixes

--- GUI/Markers.js
+++ GUI/Markers.js
@@ -1,7 +1,7 @@
 // Add markers for North Carolina, Maryland, and Wisconsin
 const northCarolinaMarker = L.marker([35.7796, -78.6382]).addTo(map);
 const marylandMarker = L.marker([39.0458, -76.6413]).addTo(map);
-const wisconsinMarker = L.marker([43.0731, -89.4012]).addTo(map);
+const wisconsinMarker = L.marker([44.0731, -89.4012]).addTo(map);
 
 // dummy data
 const stateInfo = {
@@ -18,7 +18,7 @@ const stateInfo = {
   Wisconsin: {
     name: "Wisconsin",
     description: "This is Wisconsin.",
-    latlng: [43.0731, -89.4012],
+    latlng: [44.0731, -89.4012],
   },
 };
 
@@ -26,7 +26,7 @@ const stateInfo = {
 function onMarkerClick(state) {
   const info = stateInfo[state];
   if (info) {
-    alert(`You selected ${info.name}\n\nDescription: ${info.description}`);
+    // alert(`You selected ${info.name}\n\nDescription: ${info.description}`);
   }
 }
 const stateNames = {
@@ -38,3 +38,43 @@ const stateNames = {
 northCarolinaMarker.bindTooltip(stateNames.northCarolinaMarker).openTooltip();
 marylandMarker.bindTooltip(stateNames.marylandMarker).openTooltip();
 wisconsinMarker.bindTooltip(stateNames.wisconsinMarker).openTooltip();
+
+// ------- drop down
+
+// Create a custom control for the dropdown menu
+const dropdownControl = L.control({ position: "topleft" });
+
+// Function to handle state selection from dropdown
+function onDropdownSelect(state) {
+  const info = stateInfo[state];
+  if (info) {
+    // alert(`You selected ${info.name}\n\nDescription: ${info.description}`);
+    map.setView(info.latlng, 8); // Set the map view to the clicked state's coordinates with zoom level 8
+  }
+}
+
+// Create the dropdown menu
+const dropdown = document.createElement("select");
+dropdown.id = "state-dropdown";
+dropdown.classList.add("form-control");
+dropdown.innerHTML = `
+  <option value="" disabled selected>Select a state</option>
+  <option value="NorthCarolina">North Carolina</option>
+  <option value="Maryland">Maryland</option>
+  <option value="Wisconsin">Wisconsin</option>
+`;
+
+// Event listener for state selection from the dropdown
+dropdown.addEventListener("change", function (event) {
+  const selectedState = event.target.value;
+  if (selectedState) {
+    onDropdownSelect(selectedState);
+  }
+});
+
+// Add the dropdown to the custom control
+dropdownControl.onAdd = function () {
+  return dropdown;
+};
+
+dropdownControl.addTo(map);
