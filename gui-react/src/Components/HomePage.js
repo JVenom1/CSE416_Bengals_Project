be328a3d2dfed60866cb3c3c992ba7be73500b61
Jeremy Lutsky <jlstring16@gmail.com>
2023-12-03 04:08:05 -0500

    Update HomePage.js

--- gui-react/src/Components/HomePage.js
+++ gui-react/src/Components/HomePage.js
@@ -1,52 +1,70 @@
+// description comments are used for the combination with server part (can be deleted later by anyone)
+
 import { React, useState } from "react";
 import "../App.css";
 import "leaflet/dist/leaflet.css";
 import { useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-// Data
+// Data (server call to grab this data)
 import MDOutline from "../Data/StateOutlines/MDOutline.json";
 import NCOutline from "../Data/StateOutlines/NCOutline.json";
 import WIOutline from "../Data/StateOutlines/WIOutline.json";
-// Other Files
+// District Data (once stateID retrieved make a server call to grab it, so not all needs to be loaded)
+import MDPlan from "../Data/DistrictPlans/MD.json";
+import NCPlan from "../Data/DistrictPlans/NC.json";
+import WIPlan from "../Data/DistrictPlans/WI.json";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
-  // Define the coordinates for the boundaries of Wisconsin, Maryland, and North Carolina
-  var bounds = [
+  // Handlers
+  const mapBounds = [
     [50.5, -105.0], // Wisconsin (top-left, slightly adjusted to the left)
     [30.0, -60.0], // Maryland (bottom-right)
   ];
-
-  // Style for the state boundaries (#3388ff border)
-  // const boundaryStyle = {
-  //   color: "#3388ff",
-  //   weight: 3, // Adjust the border thickness as needed
-  //   fill: true, // fill color
-  // };
-
-  // Handlers
-
-  const handleGeoJson = (data) => {
+  const handleStateOutline = (geoData) => {
     return (
       <GeoJSON
-        data={data}
+        data={geoData}
         eventHandlers={{
           click: handleStateClick,
         }}
       />
     );
   };
+  // GUI 17
   const goToHomePage = (e) => {
     navigate("/");
   };
   const handleStateClick = (e) => {
-    let stateID = e.sourceTarget.feature.properties.State;
-    navigate(`/State`, { state: { stateID: stateID } });
+    const stateID = e.sourceTarget.feature.properties.State;
+    navigate(`/EnsembleList`, {
+      state: {
+        stateID: stateID,
+        currentDistrictPlan: getCurrDistPlan(stateID),
+      },
+    });
   };
+
   const handleDropDownClick = (e) => {
-    let stateID = e.target.value;
-    navigate(`/State`, { state: { stateID: stateID } });
+    const stateID = e.target.value;
+    navigate(`/EnsembleList`, {
+      state: {
+        stateID: stateID,
+        currentDistrictPlan: getCurrDistPlan(stateID),
+      },
+    });
+  };
+  const getCurrDistPlan = (stateID) => {
+    if (stateID === "WI") {
+      return WIPlan;
+    } else if (stateID === "MD") {
+      return MDPlan;
+    } else if (stateID === "NC") {
+      return NCPlan;
+    }
+    console.log("No Data HomePage");
+    return null;
   };
   return (
     <>
@@ -55,16 +73,16 @@ const HomePage = () => {
           center={[39.67, -83.0]}
           zoom={5.5}
           minZoom={6}
-          maxBounds={bounds}
+          maxBounds={mapBounds}
           maxZoom={10}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {handleGeoJson(WIOutline)}
-          {handleGeoJson(MDOutline)}
-          {handleGeoJson(NCOutline)}
+          {handleStateOutline(WIOutline)}
+          {handleStateOutline(MDOutline)}
+          {handleStateOutline(NCOutline)}
         </MapContainer>
         <button className="home-button" onClick={goToHomePage}>
           Home
