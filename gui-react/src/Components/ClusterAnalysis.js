0ccfeb372e5019bef883eac50167ab7e5d212f63
Jeremy Lutsky <jlstring16@gmail.com>
2023-12-03 04:08:41 -0500

    stable

--- gui-react/src/Components/ClusterAnalysis.js
+++ gui-react/src/Components/ClusterAnalysis.js
@@ -3,12 +3,65 @@ import "../App.css";
 import "leaflet/dist/leaflet.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-// Data
-import MDDistricts from "../Data/DistrictPlans/MD.json";
-import NCDistricts from "../Data/DistrictPlans/NC.json";
-import WIDistricts from "../Data/DistrictPlans/WI.json";
+import magicNumbers from "../Helpers/magicNumbers.js";
+import miscFunc from "../Helpers/miscFunctions.js";
 
 const ClusterAnalysis = () => {
-    return (<></>)
-}
+  useEffect(() => {
+    changeMapSizeXbyY("100%", "25vw");
+  });
+  const changeMapSizeXbyY = (height = "100%", width = "50vw") => {
+    const leafletContainer = document.querySelector(".leaflet-container");
+    leafletContainer.style.width = width;
+    leafletContainer.style.height = height;
+  };
+  const location = useLocation();
+  const navigate = useNavigate();
+  const data = location.state.data;
+  const stateID = "WI";
+  const mapMaxBounds = magicNumbers.stateZoomBounds.stateID;
+  const mapCenter = magicNumbers.stateCenter[stateID].latlng;
+  const goToHomePage = (e) => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="mapWrapper">
+        <MapContainer
+          center={mapCenter}
+          zoom={6}
+          minZoom={6}
+          maxBounds={mapMaxBounds}
+          maxZoom={6}
+          dragging={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <GeoJSON data={state.currentDistrictPlan} /> */}
+        </MapContainer>
+        <button className="home-button" onClick={goToHomePage}>
+          Home
+        </button>
+        <MapContainer
+          center={mapCenter}
+          zoom={6}
+          minZoom={6}
+          maxBounds={mapMaxBounds}
+          maxZoom={6}
+          dragging={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <GeoJSON data={state.currentDistrictPlan} /> */}
+        </MapContainer>
+        <div className="clusterRight"></div>
+      </div>
+    </>
+  );
+};
+
 export default ClusterAnalysis;
