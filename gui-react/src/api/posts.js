ffa639b0e328b097e9f3fb02e0adee71cb83f34f
jlutsky1 <jlstring16@gmail.com>
2023-12-07 11:46:25 -0500

    Update posts.js

--- gui-react/src/api/posts.js
+++ gui-react/src/api/posts.js
@@ -5,7 +5,8 @@ import axios from "axios";
 //   baseURL: "http://localhost:8080/server/BengalsAPI",
 // });
 
+// IP 130.245.192.6
 // this is the test server
 export default axios.create({
-  baseURL: "https://flat-banks-flow.loca.lt/server/BengalsAPI",
+  baseURL: "https://cold-bottles-lie.loca.lt/server/BengalsAPI",
 });
