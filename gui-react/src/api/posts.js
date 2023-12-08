950db6a3ddd4ff668275786a1914cadc82748b14
jlutsky1 <jlstring16@gmail.com>
2023-12-08 01:06:46 -0500

    test link updated

--- gui-react/src/api/posts.js
+++ gui-react/src/api/posts.js
@@ -8,5 +8,5 @@ import axios from "axios";
 // IP 130.245.192.6
 // this is the test server
 export default axios.create({
-  baseURL: "https://cold-bottles-lie.loca.lt/server/BengalsAPI",
+  baseURL: "https://fine-zebras-agree.loca.lt//server/BengalsAPI",
 });
