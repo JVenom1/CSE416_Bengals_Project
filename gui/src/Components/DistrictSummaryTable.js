738d82924a2a79f88df0c67adf8c0846ec107bbb
Jeremy Lutsky <jlstring16@gmail.com>
2023-12-11 07:45:38 -0500

    prep for prev & next change waiting for server

--- gui/src/Components/DistrictSummaryTable.js
+++ gui/src/Components/DistrictSummaryTable.js
@@ -2,51 +2,51 @@ import "../App.css";
 import { useState } from "react";
 
 const DistrictSummaryTable = ({ distPlanList }) => {
-    const itemsPerPage = 6;
-    const [currentPage, setCurrentPage] = useState(1);
-    const [selectedPlans, setSelectedPlans] = useState([]);
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPlans, setSelectedPlans] = useState([]);
 
-    const totalPages = Math.ceil(distPlanList.length / itemsPerPage);
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
+  const totalPages = Math.ceil(distPlanList.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
-    const columnHeaders = Object.keys(distPlanList[0]);
+  const columnHeaders = Object.keys(distPlanList[0]);
 
-    const handlePrevClick = () => {
-        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-    };
+  const handlePrevClick = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
-    const handleNextClick = () => {
-        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-    };
+  const handleNextClick = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
 
-    const handleCheckboxChange = (index) => {
-        // Handle checkbox change
-        const updatedSelectedPlans = [...selectedPlans];
-        updatedSelectedPlans[index] = !selectedPlans[index];
+  const handleCheckboxChange = (index) => {
+    // Handle checkbox change
+    const updatedSelectedPlans = [...selectedPlans];
+    updatedSelectedPlans[index] = !selectedPlans[index];
 
-        // true false list where true display the plan
-        setSelectedPlans(updatedSelectedPlans);
-    };
+    // true false list where true display the plan
+    setSelectedPlans(updatedSelectedPlans);
+  };
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Availability</th>
-                        <th>Avg Oppertunity Dist Count</th>
-                        <th>Plan Name</th>
-                        <th>Splits</th>
-                        {/* {columnHeaders.map((header) => (
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Availability</th>
+            <th>Avg Oppertunity Dist Count</th>
+            <th>Plan Name</th>
+            <th>Splits</th>
+            {/* {columnHeaders.map((header) => (
                             <th key={header}>{header}</th>
                         ))} */}
-                    </tr>
-                </thead>
-                <tbody>
-                    {distPlanList.slice(startIndex, endIndex).map((distPlan, index) => (
-                        <tr key={index}>
-                            {/* <td>
+          </tr>
+        </thead>
+        <tbody>
+          {distPlanList.slice(startIndex, endIndex).map((distPlan, index) => (
+            <tr key={index}>
+              {/* <td>
                                 {distPlan.availability === "Available" ? (
                                     <input
                                         type="checkbox"
@@ -57,33 +57,35 @@ const DistrictSummaryTable = ({ distPlanList }) => {
                                     "X"
                                 )}
                             </td> */}
-                            {columnHeaders.map((header) => (
-                                <td key={header}>{distPlan[header]}</td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+              {columnHeaders.map((header) => (
+                <td key={header}>{distPlan[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-            <div>
-                <button
-                    onClick={handlePrevClick}
-                    disabled={currentPage === 1}
-                    className={currentPage === 1 ? "disabled-button" : ""}
-                >
-                    Prev
-                </button>
-                <span>{`Page ${currentPage} of ${totalPages}`}</span>
-                <button
-                    onClick={handleNextClick}
-                    disabled={currentPage === totalPages}
-                    className={currentPage === totalPages ? "disabled-button" : ""}
-                >
-                    Next
-                </button>
-            </div>
-        </div>
-    );
+      <div>
+        <button
+          onClick={handlePrevClick}
+          disabled={currentPage === 1}
+          className={currentPage === 1 ? "disabled-button" : "default-button"}
+        >
+          Prev
+        </button>
+        <span>{`Page ${currentPage} of ${totalPages}`}</span>
+        <button
+          onClick={handleNextClick}
+          disabled={currentPage === totalPages}
+          className={
+            currentPage === totalPages ? "disabled-button" : "default-button"
+          }
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DistrictSummaryTable;
--- gui/src/Components/EnsembleTable.js
+++ gui/src/Components/EnsembleTable.js
@@ -1,165 +1,196 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import api from "../api/posts.js"
-const EnsembleTable = ({ headerText, ensembleDetails, ensembleTableWidth, ensembleTableHeight, stateID, currentDistrPlan }) => {
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/posts.js";
+const EnsembleTable = ({
+  headerText,
+  ensembleDetails,
+  ensembleTableWidth,
+  ensembleTableHeight,
+  stateID,
+  currentDistrPlan,
+}) => {
+  const navigate = useNavigate();
+  const rowsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const navigate = useNavigate();
-    const rowsPerPage = 3;
-    const [currentPage, setCurrentPage] = useState(1);
-
-    const startIdx = (currentPage - 1) * rowsPerPage;
-    const endIdx = startIdx + rowsPerPage;
-    const displayedEnsembleDetails = ensembleDetails.slice(startIdx, endIdx);
-    const clusterScatterWidth = window.innerWidth * 0.5; // 50% of the screen width
-    const clusterScatterHeight = window.innerHeight;
-    const handlePrevClick = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
-
-    const handleNextClick = () => {
-        const totalPages = Math.ceil(ensembleDetails.length / rowsPerPage);
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
-    const getClusterSum = async (stateID, ensembleIndex) => {
-        try {
-            const response = await api.get(`/${stateID}/${ensembleIndex}/inital_summary`);
-            const ensembleSum = response.data;
-            return ensembleSum;
-        } catch (err) {
-            console.log(err)
-            return null;
-        }
+  const startIdx = (currentPage - 1) * rowsPerPage;
+  const endIdx = startIdx + rowsPerPage;
+  const displayedEnsembleDetails = ensembleDetails.slice(startIdx, endIdx);
+  const clusterScatterWidth = window.innerWidth * 0.5; // 50% of the screen width
+  const clusterScatterHeight = window.innerHeight;
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
     }
-    const getClusterDetails = async (stateID, ensembleIndex) => {
-        try {
-            const response = await api.get(`/${stateID}/${ensembleIndex}/cluster_details`);
-            const clusters = response.data;
-            // console.log(clusters)
-            return clusters;
+  };
 
-        } catch (err) {
-            console.log(err)
-            return null;
-        }
+  const handleNextClick = () => {
+    const totalPages = Math.ceil(ensembleDetails.length / rowsPerPage);
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
     }
+  };
+  const getClusterSum = async (stateID, ensembleIndex) => {
+    try {
+      const response = await api.get(
+        `/${stateID}/${ensembleIndex}/inital_summary`
+      );
+      const ensembleSum = response.data;
+      return ensembleSum;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  };
+  const getClusterDetails = async (stateID, ensembleIndex) => {
+    try {
+      const response = await api.get(
+        `/${stateID}/${ensembleIndex}/cluster_details`
+      );
+      const clusters = response.data;
+      // console.log(clusters)
+      return clusters;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  };
 
-    const getClustCoords = async (stateID, ensemIndex) => {
-        try {
-            const response = await api.get(`/${stateID}/${ensemIndex}/cluster_coordinates`);
-            const clusterSum = response.data;
-            return clusterSum;
-        } catch (error) {
-            console.log(error);
-        }
+  const getClustCoords = async (stateID, ensemIndex) => {
+    try {
+      const response = await api.get(
+        `/${stateID}/${ensemIndex}/cluster_coordinates`
+      );
+      const clusterSum = response.data;
+      return clusterSum;
+    } catch (error) {
+      console.log(error);
     }
-    const getAssocCoords = async (stateID, ensemIndex) => {
-        try {
-            const response = await api.get(`/${stateID}/${ensemIndex}/cluster_association_coordinates`);
-            const clusterAssoc = response.data;
-            return clusterAssoc;
-        } catch (error) {
-            console.log(error);
-        }
+  };
+  const getAssocCoords = async (stateID, ensemIndex) => {
+    try {
+      const response = await api.get(
+        `/${stateID}/${ensemIndex}/cluster_association_coordinates`
+      );
+      const clusterAssoc = response.data;
+      return clusterAssoc;
+    } catch (error) {
+      console.log(error);
     }
-    const getAllDistanceMeasures = async (stateID, ensembleIndex) => {
-        try {
-            const response = await api.get(`/${stateID}/${ensembleIndex}/distance_measures`);
-            return response.data;
-        }
-        catch (error) {
-            console.log(error)
-        }
+  };
+  const getAllDistanceMeasures = async (stateID, ensembleIndex) => {
+    try {
+      const response = await api.get(
+        `/${stateID}/${ensembleIndex}/distance_measures`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
     }
-    const handleDistanceMeasClick = async (ensembleName) => {
-        const ensembleIndex = parseInt(ensembleName.charAt(ensembleName.length - 1), 10) - 1;
-        document.body.style.cursor = 'wait';
-        const allDistanceMeasuresMatrix = await getAllDistanceMeasures(stateID, ensembleIndex);
-        navigate("/DistMatrixTable",
-            {
-                state:
-                {
-                    stateID: stateID,
-                    headerText: headerText,
-                    currentDistrPlan: currentDistrPlan,
-                    matrixList: allDistanceMeasuresMatrix
-                }
-            });
-    }
-    const handleEnsemClick = async (ensembleName) => {
-        const ensembleIndex = parseInt(ensembleName.charAt(ensembleName.length - 1), 10) - 1;
-        // then the details like Hispanic population vs black population
-        document.body.style.cursor = 'wait';
-        const clusterCoords = await getClustCoords(stateID, ensembleIndex);
-        // pass list of cluster in said ensemble
-        const clusterSum = await getClusterSum(stateID, ensembleIndex);
-        const clusterAssocCoords = await getAssocCoords(stateID, ensembleIndex);
-        const clusters = await getClusterDetails(stateID, ensembleIndex);
-        navigate("/ClusterAnalysis",
-            {
-                state: {
-                    stateID: stateID,
-                    headerText: headerText,
-                    currentDistrPlan: currentDistrPlan,
-                    clusters: clusters,
-                    clusterCoords: clusterCoords,
-                    clusterScatterWidth: clusterScatterWidth,
-                    clusterScatterHeight: clusterScatterHeight,
-                    clusterSum: clusterSum,
-                    ensembleName: ensembleName,
-                    ensembleIndex: ensembleIndex,
-                    clusterAssocCoords: clusterAssocCoords,
-                    //  matrix soon to have 3 key:value pairs for the 3 distances (now its just data)
-                    //  .data=array[ixi]
-
-                }
-            });
-    }
-
-    return (
-        <div className="table-container" >
-            <table style={{ padding: '10px' }}>
-                <thead>
-                    <tr>
-                        <th>Ensemble Name</th>
-                        <th>Ensemble Size</th>
-                        <th>Cluster Count</th>
-                        <th>More Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {displayedEnsembleDetails.map((row, index) => (
-                        <tr key={index}>
-                            <td>{row.name}</td>
-                            <td>{row.ensemblesize}</td>
-                            <td>{row.clustercount}</td>
-                            <td>
-                                <button onClick={() => handleEnsemClick(row.name)}>
-                                    {`Cluster Analysis`}
-                                </button>
-                                <button onClick={() => handleDistanceMeasClick(row.name)}>
-                                    {`Distance Measures`}
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            <div style={{ padding: '10px' }}>
-                <button className="pagination-btn" onClick={handlePrevClick} disabled={currentPage === 1}>
-                    Prev
-                </button>
-                <span style={{ padding: '5px' }}>{`Page ${currentPage}`}</span>
-                <button className="pagination-btn" onClick={handleNextClick} disabled={currentPage === Math.ceil(ensembleDetails.length / rowsPerPage)}>
-                    Next
-                </button>
-            </div>
-        </div>
+  };
+  const handleDistanceMeasClick = async (ensembleName) => {
+    const ensembleIndex =
+      parseInt(ensembleName.charAt(ensembleName.length - 1), 10) - 1;
+    document.body.style.cursor = "wait";
+    const allDistanceMeasuresMatrix = await getAllDistanceMeasures(
+      stateID,
+      ensembleIndex
     );
+    navigate("/DistMatrixTable", {
+      state: {
+        stateID: stateID,
+        headerText: headerText,
+        currentDistrPlan: currentDistrPlan,
+        matrixList: allDistanceMeasuresMatrix,
+      },
+    });
+  };
+  const handleEnsemClick = async (ensembleName) => {
+    const ensembleIndex =
+      parseInt(ensembleName.charAt(ensembleName.length - 1), 10) - 1;
+    // then the details like Hispanic population vs black population
+    document.body.style.cursor = "wait";
+    const clusterCoords = await getClustCoords(stateID, ensembleIndex);
+    // pass list of cluster in said ensemble
+    const clusterSum = await getClusterSum(stateID, ensembleIndex);
+    const clusterAssocCoords = await getAssocCoords(stateID, ensembleIndex);
+    const clusters = await getClusterDetails(stateID, ensembleIndex);
+    navigate("/ClusterAnalysis", {
+      state: {
+        stateID: stateID,
+        headerText: headerText,
+        currentDistrPlan: currentDistrPlan,
+        clusters: clusters,
+        clusterCoords: clusterCoords,
+        clusterScatterWidth: clusterScatterWidth,
+        clusterScatterHeight: clusterScatterHeight,
+        clusterSum: clusterSum,
+        ensembleName: ensembleName,
+        ensembleIndex: ensembleIndex,
+        clusterAssocCoords: clusterAssocCoords,
+        //  matrix soon to have 3 key:value pairs for the 3 distances (now its just data)
+        //  .data=array[ixi]
+      },
+    });
+  };
+
+  return (
+    <div className="table-container">
+      <table style={{ padding: "10px" }}>
+        <thead>
+          <tr>
+            <th>Ensemble Name</th>
+            <th>Ensemble Size</th>
+            <th>Cluster Count</th>
+            <th>More Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayedEnsembleDetails.map((row, index) => (
+            <tr key={index}>
+              <td>{row.name}</td>
+              <td>{row.ensemblesize}</td>
+              <td>{row.clustercount}</td>
+              <td>
+                <button
+                  className="default-button"
+                  onClick={() => handleEnsemClick(row.name)}
+                >
+                  {`Cluster Analysis`}
+                </button>
+                <button
+                  className="default-button"
+                  onClick={() => handleDistanceMeasClick(row.name)}
+                >
+                  {`Distance Measures`}
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div style={{ padding: "10px" }}>
+        <button
+          className="pagination-btn"
+          onClick={handlePrevClick}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+        <span style={{ padding: "5px" }}>{`Page ${currentPage}`}</span>
+        <button
+          className="pagination-btn"
+          onClick={handleNextClick}
+          disabled={
+            currentPage === Math.ceil(ensembleDetails.length / rowsPerPage)
+          }
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default EnsembleTable;
