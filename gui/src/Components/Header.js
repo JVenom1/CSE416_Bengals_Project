30ab4daac9afeb58268cbf12a6f2896a8a82ece1
Jeremy Lutsky <jlstring16@gmail.com>
2023-12-11 07:45:13 -0500

    removed redo

--- gui/src/Components/Header.js
+++ gui/src/Components/Header.js
@@ -2,37 +2,38 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const Header = ({ headerText }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const goToHomePage = () => {
-        navigate("/");
-    };
+  const goToHomePage = () => {
+    navigate("/");
+  };
 
-    const undoPage = () => {
-        window.history.back();
-    };
+  const undoPage = () => {
+    window.history.back();
+  };
 
-    const redoPage = () => {
-        window.history.forward();
-    };
+  const redoPage = () => {
+    window.history.forward();
+  };
 
-    return (
-        <>
-            <div className="header">
-                <div className="header-button-list">
-                    <button onClick={goToHomePage} className="header-button">
-                        Reset
-                    </button>
-                    <button onClick={undoPage} className="header-button">
-                        Undo Page
-                    </button>
-                    <button onClick={redoPage} className="header-button">
+  return (
+    <>
+      <div className="header">
+        <div className="header-button-list">
+          <button onClick={goToHomePage} className="header-button">
+            Reset
+          </button>
+          <button onClick={undoPage} className="header-button">
+            Undo Page
+          </button>
+          {/* <button onClick={redoPage} className="header-button">
                         Redo Page
-                    </button></div>
-                <span className="header-text">{headerText}</span>
-            </div>
-        </>
-    );
+                    </button>*/}
+        </div>
+        <span className="header-text">{headerText}</span>
+      </div>
+    </>
+  );
 };
 
 export default Header;
