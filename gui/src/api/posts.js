df648f67d499f85ea6f4501f7fa24fd3bce81c8b
Jeremy Lutsky <jlstring16@gmail.com>
2024-01-08 23:18:57 -0500

    updated api

--- gui/src/api/posts.js
+++ gui/src/api/posts.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // this is the main server
-// export default axios.create({
-//   baseURL: "http://localhost:8080/server/BengalsAPI",
-// });
+export default axios.create({
+  baseURL: "http://localhost:8080/server/BengalsAPI",
+});
 
 // IP 130.245.192.6
 // this is the test server
-const tempServer = `https://six-masks-bow.loca.lt/server/`;
-export default axios.create({
-  baseURL: `${tempServer}BengalsAPI`,
-});
+// const tempServer = `https://six-masks-bow.loca.lt/server/`;
+// export default axios.create({
+//   baseURL: `${tempServer}BengalsAPI`,
+// });
